3cbd6bdd5afad3f3dc962d7745af349bf59d430b
stamander <iida.atsushi.fh@gmail.com>
2020-09-03 07:37:18 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,17 +1,8 @@
-years_list = [1996,1997,1998,1999,2000,2001]
-
-print(years_list[5])
-
-things = ['mozzarella','chinderella','salmonella'
-]
-
-things[0]=things[0].upper()
-
-things.remove("salmonella")
-print(things)
 
 
 
+surprise = ['Groucho','Chico','Harpo']
+print(surprise)
 
 
 
