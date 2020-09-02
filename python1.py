31462a81309c525be7538b060430d1d3acd2bb1d
stamander <iida.atsushi.fh@gmail.com>
2020-09-03 08:12:24 +0900

    Update python1.py

---
+++ python1.py
@@ -0,0 +1,13 @@
+life = {
+  'animals':{
+    'cats':[
+      'Henri','Grumpy','Lucy'
+    ],
+    'ocotopi':{},
+    'emus':{}
+  },
+  'plants':{},
+  'other':{}
+  }
+
+print(life['animals']['cats'])
