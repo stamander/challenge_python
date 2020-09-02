1efb966d903987aaff7740fda9e0a73477e26c87
stamander <iida.atsushi.fh@gmail.com>
2020-09-02 10:55:36 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,12 +1,5 @@
 
-bierce = {
-  "day": "A period of twenty-four hours, mostly misspent",
-  "possitive":"Mistaken at the top of one's voice",
-  "misfortune":"The kind of fortune that never misses",
-}
 
-lot = [('a','b'),('c','d'),('e','f')]
-print(dict(lot))
 
 pythons = {
   'Champman':'Graham',
