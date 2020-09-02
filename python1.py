4826f10242d47165038da350dee29e79518f6516
stamander <iida.atsushi.fh@gmail.com>
2020-09-02 13:05:37 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,15 +1,2 @@
-drinks = {
-  'martini':{'vodka':'vermouth'},
-  'black russian':{'vodka','kahlua'},
-  'white russian':{'cream','kahlua','vodka'},
-  'manhattan':{'rye','vermouth','bitters'},
-  'screwdriver':{'orange juice','vodka'}
-}
-
-
-
-for name,contents in drinks.items():
-  if 'vodka' in contents:
-    print(name)
-    
-
+marx_list =['Groucho','Chico','Harpo']
+marx_tuple = 'Groucho','Chico','Ha'
