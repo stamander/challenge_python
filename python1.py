8660673b6e52b1e7a6505dc9d18f9aa7fd93d5a1
stamander <iida.atsushi.fh@gmail.com>
2020-09-02 12:10:41 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,8 +1,3 @@
-empty_set = set()
-print(empty_set)
+print(set(('Ummagunma','Echoes','Atom Herat Mother')))
 
-even_numbers = {0,2,4,6,8}
-print(even_numbers)
-
-odd_numbers = {1,3,5,7,9}
-print(odd_numbers)
+print(set({'apple':'red','orange':'orange','cherry':'red'})) 
