1e91f5720a0ce6f9cf32bc02c9135d5e64ad3636
stamander <iida.atsushi.fh@gmail.com>
2020-09-03 07:12:31 +0900

    Update python1.py

--- python1.py
+++
@@ -1,8 +0,0 @@
-marxes =['Groucho','Chico','Harpo']
-pythons = ['Champman','Cleese','Gilliam','Jones','Palin']
-strooges= ['Moe','Curry','Larry']
-
-
-dict_of_lists = ['Maxes' marxes,'Pythons'pythons,'Stooges':strooges]
-
-print(dict_of_lists)
