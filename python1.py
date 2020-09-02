51e280a03ba05129e054d237590c9fea8ae21ee0
stamander <iida.atsushi.fh@gmail.com>
2020-09-02 10:37:31 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,1 +1,6 @@
 
+bierce = {
+  "day": "A period of twenty-four hours, mostly misspent",
+  "possitive":"Mistaken at the top of one's voice",
+  "misfortune":"The kind of fortune that never misses",
+}
