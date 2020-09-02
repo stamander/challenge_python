57cca05b47eca117f901be96bef785eea2b3e45d
stamander <iida.atsushi.fh@gmail.com>
2020-09-02 10:31:08 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,12 +1,1 @@
 
-empty_list= [ ]
-weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
-big_birds = ['emu','ostrich','cassowary']
-first_names = ['Graham','John','Terry','Terry','Michael']
-
-birtday = '1/6/1997'
-birtday.split('/')
-
-
-list(big_birds)
-print(big_birds)
