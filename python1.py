2554ee29f4b2ae0346c9ee99c2a0ac79dee84331
stamander <iida.atsushi.fh@gmail.com>
2020-09-03 16:29:32 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,9 +1,15 @@
 
 # -*- coding: utf-8 -*-
 
-def even_odd(x):
+
+
+def even_odd():
+  x = input("number please:")
+  x = int(x)
   if x % 2 == 0:
     print("偶数")
   else:
     print("奇数")
-    
+
+
+even_odd()
