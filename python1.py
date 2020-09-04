a9f7cd2e3a025da2023bfb202d0136754e7ff5c4
stamander <iida.atsushi.fh@gmail.com>
2020-09-05 08:40:15 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -3,12 +3,3 @@
  
 
 
-try:
-  a = input("type a number:")
-  b = input("type b number:")
-
-  a = int(a)
-  b = int(b)
-  print (a/b)
-except (ZeroDivisionError,ValueError):
-  print("bに0を入れたり、数値データを入れたりしたでしょ?だから計算できないよ")
