38fbc61ca04556c4fcf263904eeb8bd18cfedc57
stamander <iida.atsushi.fh@gmail.com>
2020-09-07 19:20:17 +0900

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,8 +1,11 @@
+x = 7 * 10
+y = 5 * 6
 
-# -*- coding: utf-8 -*-
+# xが70と等しい場合に「xは70です」と出力してください
+if x == 70:
+    print("xは70です")
 
-fruit = ["Appple","Orange","Pear"]
 
-fruit[0]
-fruit[1]
-fruit[2]
+# yが40と等しくない場合に「yは40ではありません」と出力してください
+if y != 40:
+    print("yは40ではありません")
