03feb87083a7b4dea0ec538662efc81740be811b
stamander <iida.atsushi.fh@gmail.com>
2020-09-10 14:52:16 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,1 +1,9 @@
-print("Cat" in "cat in the hat")
+shows = ["GOT","Narcos","Vice"]
+for show in shows:
+  print(show)
+
+
+
+coms = ("A.Development","Friends","Always  Sunny")
+for com in coms:
+  print(com)
