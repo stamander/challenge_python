67846697bfd4e2e03d45ab8f2bf04c7d99c01e7c
stamander <iida.atsushi.fh@gmail.com>
2020-09-11 13:18:25 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,13 +1,22 @@
-qs = [
-  "What yor name?"
-  "What is favorite color?"
-  "What is quest?"
-]
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.animation import ArtistAnimation
 
-n = 0
-while True:
-  print("Type q to quit")
-  a = input(qs[n])
-  if a == "q":
-    break
-  n = (n+1)%3
+fig, ax = plt.subplots()
+ax.set_xlim(2018, 2020)
+ax.set_ylim(0, 500)
+
+artists = []
+for index, value in enumerate([2019, 2020]):
+    if index % 2 == 0:
+        years = [2018, 2019]
+        year_money = [100, 300]
+    else:
+        years = [2018, 2019, 2020]
+        year_money = [100, 300, 200]
+    plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))  # Xを整数で表示
+    artists.append(ax.plot(years, year_money, lw=5))
+
+animation = ArtistAnimation(fig, artists, interval=500, repeat_delay=1000)
+
+plt.show()
