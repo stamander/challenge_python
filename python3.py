a6a2ff6ebbbffc1f6855177efc4d970af4e1a9aa
stamander <iida.atsushi.fh@gmail.com>
2020-09-11 11:29:52 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,3 +1,13 @@
-for i in range (0,100):
-  print(i)
-  break
+qs = [
+  "What yor name?"
+  "What is favorite color?"
+  "What is quest?"
+]
+
+n = 0
+while True:
+  print("Type q to quit")
+  a = input(qs[n])
+  if a == "q":
+    break
+  n = (n+1)%3
