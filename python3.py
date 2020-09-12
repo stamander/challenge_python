18bba1ec5ba590ca12a6dfce657cb4dca8285216
stamander <iida.atsushi.fh@gmail.com>
2020-09-13 07:55:20 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,9 +1,4 @@
-import statistics
+import keyword
 
-nums = [1,5,33,12,46,33,2]
-
-print(statistics.mean(nums))
-
-print(statistics.median(nums))
-
-print(statistics.mode(nums))
+print(keyword.iskeyword("for"))
+print(keyword.iskeyword("footboll"))
