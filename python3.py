88dc3edaf13cde81593b929450304e15bcddf80a
stamander <iida.atsushi.fh@gmail.com>
2020-09-13 07:50:58 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,3 +1,9 @@
-import  random
+import statistics
 
-print(random.randint(0,100))
+nums = [1,5,33,12,46,33,2]
+
+print(statistics.mean(nums))
+
+print(statistics.median(nums))
+
+print(statistics.mode(nums))
