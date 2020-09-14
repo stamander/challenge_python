c9b010f876f475c9feb9565c00044e9fbaff1082
stamander <iida.atsushi.fh@gmail.com>
2020-09-14 14:12:30 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -13,4 +13,18 @@ def hangman(word):
 
 
 
-  rl
+  rletters = list(word)
+  board = ["_"] * len(word)
+  win = False
+  print("ハングマンへようこそ！")
+
+
+while wrong < len (stages) -1:
+  print("!\n")
+  msg ="1文字を予想してね"
+  char = input(msg)
+  if char in rletters:
+    cind = rletters.index(char)
+    borad[cind] = char
+    rletters[cind]= '$'
+  
