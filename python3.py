70f44e35c21b79a213b25c221691771dba5ddc35
stamander <iida.atsushi.fh@gmail.com>
2020-09-15 12:43:27 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -19,16 +19,29 @@ def hangman(word):
   print("ハングマンへようこそ！")
 
 
-while wrong < len (stages) -1:
-  print("!\n")
-  msg ="1文字を予想してね"
-  char = input(msg)
-  if char in rletters:
-    cind = rletters.index(char)
-    borad[cind] = char
-    rletters[cind]= '$'
-  else:
-    wrong +=1
-    print(" ".join)
+  while wrong < len (stages) -1:
+    print("!\n")
+    msg ="1文字を予想してね"
+    char = input(msg)
+    if char in rletters:
+      cind = rletters.index(char)
+      borad[cind] = char
+      rletters[cind]= '$'
+    else:
+      wrong +=1
+      print(" ".join(borad))
+      e = wrong +1
+      print("\n".join(stages[0:e]))
+      if "_" not in board:
+        print("あなたの勝ち！")
+        print (" ".join(board))
+        win = True
+        break
+  if not win:
+    print("\n.join"(stages[0:wrong + 1]))
+    print("あなたの負け!正解は{}.".format(word))
+
+hangman("cat")
+      
 
   
