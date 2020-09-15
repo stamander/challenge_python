9230c0c6bbec786b2bd96518fcd013e307db3754
stamander <iida.atsushi.fh@gmail.com>
2020-09-16 08:20:56 +0900

    Update python4.py

--- python4.py
+++ python4.py
@@ -1,7 +1,4 @@
-test1 = (90,92,76,86,67)
-sum = test1[0]+ test1[1] + test1[2]+test1[3]+test1[4]
-test1[4] = 21
-print(sum)
-avg = sum/5
-
-print(avg)
+test1 = ((90,92,76,87,67),(89,77,56,81,79),(67,86,71,65,57))
+print(test1[0])
+print(test1[1])
+print(test1[2])
