0998743b1508cb8bef60de49fc60fbf5d6f0e1db
stamander <iida.atsushi.fh@gmail.com>
2020-09-15 19:06:31 +0900

    Update python4.py

--- python4.py
+++ python4.py
@@ -1,6 +1,8 @@
-def calc_tax(price):
-  tax_inc_price = price * 1.08
+def calc_tax(price,tax_rate):
+  tax_inc_price = price * (1 +tax_rate)
+
   return tax_inc_price
 
 
-print(calc_tax(2000))
+print(calc_tax(2000,0.08),"円")
+print(calc_tax(2000,0.1),"円")
