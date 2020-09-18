ea26cb109d67c8509e7eb21c90ef493ed3921e2b
stamander <iida.atsushi.fh@gmail.com>
2020-09-18 17:55:34 +0900

    Update python4.py

--- python4.py
+++ python4.py
@@ -1,7 +1,10 @@
-a = False
-if a:
-  print("Black start")
-  print("-----------")
-  print("Black end")
+math = 67
 
-print("out of Block")
+if math >= 60:
+  print("True block start")
+  print("good")
+  print("True block end")
+else:
+  print("True block start")
+  print("bad")
+  print("True block end")
