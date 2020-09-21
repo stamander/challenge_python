0051d20eda2f5fb449f84a4aed619e06c1426bff
stamander <iida.atsushi.fh@gmail.com>
2020-09-21 19:17:41 +0900

    Update python4.py

--- python4.py
+++ python4.py
@@ -1,8 +1,23 @@
-def sub(x ,y):
-  return x - y
+from random import randint
+
+def get_anser():
+  while True:
+    ans = input("High(H) or Low(L)>")
+    if ans.upper() == "H" or ans.upper()== "L":
+      break
+  return ans.upper()
+
 
 def main():
-  print(sub(5,2))
+  player_count = 0
+  computer_count = 0
 
-if __name__ == '__main__':
-  main()
+  while player_count < 5 and computer_count <5:
+    player_number = randint(1,13)
+    print("Your number:{}".format(player_number))
+    computer_number = randint(1,13)
+    print("Computer number:?")
+    anser = get_anser()
+    balance = computer_number - player_number
+    if balance == 0:
+      print("even")
