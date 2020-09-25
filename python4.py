d517b7075dffcfb77b93c08b9832f69946fca764
stamander <iida.atsushi.fh@gmail.com>
2020-09-25 09:54:52 +0900

    pygame

--- python4.py
+++ python4.py
@@ -4,15 +4,15 @@ from pygame.locals import QUIT
 
 
 def main():
-  pygame.init()
-  surface = pygame.display.set_mode((300,300))
-  pygame.display.set_caption("Hello pygame")
-  clock = pygame.time.Clock()
-  font = pygame.font.Font(None, 30)
-  text = font.render("Hello Pygame",True,(10,10,10))
+  pygame.init() #pygame初期化
+  surface = pygame.display.set_mode((300,300)) #描写用のウインドウ初期化
+  pygame.display.set_caption("Hello pygame") #ウィンドウにタイトルを設定
+  clock = pygame.time.Clock() #クロックオブジェクトでフレームレイトを指定
+  font = pygame.font.Font(None, 30) #filename,sizeの順 ここではnoneを指定
+  text = font.render("Hello Pygame",True,(10,10,10)) #renderメソッドを使って指定した文字を新規Surfaceに追加
   textpos = text.get_rect()
-  textpos.centerx = surface.get_rect().centerx
-  textpos.centery = surface.get_rect().centery
+  textpos.centerx = surface.get_rect().centerx #x座標の中心
+  textpos.centery = surface.get_rect().centery #y座標の中心
 
   while True:
     for event in pygame.event.get():
---
+++ python5.py
@@ -0,0 +1,8 @@
+import sys 
+import pygame
+from pygame.locals import QUIT
+
+def main():
+  pygame.init() #pygame初期化
+  surface = pygame.display.set((300,400)) #描写用のウインドウ初期化
+  
