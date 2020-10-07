e7d4b1b2dc6b41b72c5aff65e3d9544f159bea39
stamander <iida.atsushi.fh@gmail.com>
2020-10-07 15:18:56 +0900

    Update python8.py

--- python8.py
+++ python8.py
@@ -2,7 +2,10 @@ foods = ['pasta','curry','sushi']
 
 foods.append('pizza')
 
-
-
 for food in foods:
   print("my favorite food is " + food)
+
+
+disney={'mickey':'mouse','donald':'duck','atsushi':'human'}
+
+print("僕らのクラブのリーダーは"+disney['mickey'])
