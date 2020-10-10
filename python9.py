032319ef778796cbe7b83c404fc93884fed771c1
stamander <iida.atsushi.fh@gmail.com>
2020-10-10 13:44:42 +0900

    Update python9.py

--- python9.py
+++ python9.py
@@ -1,15 +1,8 @@
 from menu_item import MenuItem
 
-
-
-
-
 menu_item1 = MenuItem('サンドイッチ',500)
 menu_item2 = MenuItem('チョコケーキ',400)
+menu_item3 = MenuItem('コーヒー', 300)
+menu_item4 = MenuItem('オレンジジュース', 200)
 
-menu_item1.info()
-
-menu_item2.info()
-
-result = menu_item1.total_price(4)
-print('合計:' + str(result))
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
