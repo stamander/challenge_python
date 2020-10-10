6323b5eb5ffe4c455599cc5e1533b27a2e702aa4
stamander <iida.atsushi.fh@gmail.com>
2020-10-10 13:30:32 +0900

    Update python9.py

--- python9.py
+++ python9.py
@@ -1,9 +1,26 @@
-class ManuItem:
-  pass
+class MenuItem:
+    
+  def info(self):
+    print(self.name + ':¥' + str(self.price))
 
+  def total_price(self,count):
+    get_total_price = self.price * count
+    return get_total_price
 
-menu_item1 = ManuItem() 
-menu_item1.name  = 'サンドウィッチ'
+menu_item1 = MenuItem()
+menu_item1.name = 'サンドイッチ'
 menu_item1.price = 500
 
-print(menu_item1.name)
+
+menu_item1.info()
+
+
+menu_item2 = MenuItem()
+menu_item2.name = 'チョコケーキ'
+menu_item2.price = 400
+
+
+menu_item2.info()
+
+result = menu_item1.total_price(4)
+print('合計:' + str(result))
