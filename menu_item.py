31a68eb0c5c175ebda6d7382516e911cd9f41377
stamander <iida.atsushi.fh@gmail.com>
2020-10-11 12:41:04 +0900

    Update menu_item.py

--- menu_item.py
+++ menu_item.py
@@ -1,17 +1,15 @@
 class MenuItem:
-  def __init__(self,name,price ):
-    self.name = name
-    self.price = price
-    
-  def info(self):
-    return self.name + ':¥' + str(self.price)
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-  def total_price(self,count):
-    get_total_price = self.price * count
-    
+    def info(self):
+        return self.name + ': ¥' + str(self.price)
 
-    if count >= 3:
-      get_total_price *= 0.9
+    def get_total_price(self, count):
+        total_price = self.price * count
 
-    return round(get_total_price)
+        if count >= 3:
+            total_price *= 0.9
 
+        return round(total_price)
