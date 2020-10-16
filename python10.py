bb170e8fb5e9a84edbeafe30df432589e9407ce5
stamander <iida.atsushi.fh@gmail.com>
2020-10-16 10:46:52 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,8 +1,11 @@
-Balance_money = int(input())
+# person = int(input('人数'))
 
+# for persons in range(person):
+#   persons=1
+#   persons= persons+persons
+#   print(persons)
 
-if Balance_money < 10000:
-  Balance_money+=10000
-  print(Balance_money)
-else:
-  print(Balance_money)
+n = int(input())
+n+=1
+print(sum(range(1,n)))
+ 
