60587753fbd8e41bead30556323f0d52570be043
stamander <iida.atsushi.fh@gmail.com>
2020-10-16 11:04:44 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,11 +1,3 @@
-# person = int(input('人数'))
-
-# for persons in range(person):
-#   persons=1
-#   persons= persons+persons
-#   print(persons)
-
-n = int(input())
-n+=1
-print(sum(range(1,n)))
- 
+a,b=(int(x) for x in input().split())
+print(a)
+print(b)
