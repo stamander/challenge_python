f93887ef6fa3d4c054bbd72ccac39d20bbb153f3
stamander <iida.atsushi.fh@gmail.com>
2020-10-18 19:04:02 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,2 +1,25 @@
-a,b=input().split()
-print(a[:1]+'.'+b[:1])
+
+
+N = int(input()) #1行目のNを取得する
+s = [int(input()) for i in range(N)] #複数行の数値の入力を取得
+
+for num in s:
+
+  
+
+  answer = [i for i in range(1, num+1) if num % i ==0]
+
+
+
+  total_answer = sum(answer)-num
+
+
+
+  if num == total_answer:
+    print('perfect')
+
+  elif num - total_answer == 1:
+    print('nearly')
+
+  else:
+    print('neither')
