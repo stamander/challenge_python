0c677b42d085514467cc9f3c5107bcb3e6e885b3
stamander <iida.atsushi.fh@gmail.com>
2020-10-21 12:23:39 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,19 +1,2 @@
 
 
-i = list(map(int, input().split()))
-
-N = int(i[0])
-Decimal_number = int(i[1])
-
-select_number = [int(input()) for i in range(N)]
-
-
-
-Binary_number = bin(Decimal_number)[2:]
-
-str_Binary_number = str(Binary_number)
-
-for select_numbers in select_number :
-
-  print(str_Binary_number[-select_numbers])
-
