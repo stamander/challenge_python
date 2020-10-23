a0446e538474f8855e6dc3d1ab1e15d6c227894c
stamander <iida.atsushi.fh@gmail.com>
2020-10-23 10:18:52 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,3 +1,10 @@
-N, M = map(int, input().split())
+i = list(map(int, input().split()))
+print(i)
 
-print(N,M)
+character_a = i[0]
+character_b = i[1]
+
+a = 0
+b = 0
+
+print((a+character_a)-(b+character_b))
