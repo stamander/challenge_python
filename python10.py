75eaef77a723a97a871d85fea8d2b56a1279ca0e
stamander <iida.atsushi.fh@gmail.com>
2020-10-23 11:23:00 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,10 +1,45 @@
-i = list(map(int, input().split()))
-print(i)
 
-character_a = i[0]
-character_b = i[1]
+input_line = (input())
+a = input_line[:1]
+kigou = input_line[2:3]
+b = input_line[4:5]
+c = input_line[8:9]
 
-a = 0
-b = 0
 
-print((a+character_a)-(b+character_b))
+
+
+
+
+if a == 'x':
+  b = int(b)
+  c = int(c)
+
+  if kigou == '+':
+    answer = c -b
+  elif kigou == '-':
+    answer = c+b
+
+elif b == 'x':
+  a = int(a)
+  c = int(c)
+  if kigou == '+':
+    answer = c+a
+  elif kigou == '-':
+    answer  = (c - a) /-1
+
+    answer=int(answer)
+
+
+elif c == 'x':
+  a = int(a)
+  b = int(b)
+
+  if kigou == '+':
+    answer = a + b
+  elif kigou == '-':
+    answer = a-b
+
+print(answer)
+
+
+
