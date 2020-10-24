5a6be6560b471f3ec7ca6ea539e1972cc78f678e
stamander <iida.atsushi.fh@gmail.com>
2020-10-24 11:27:38 +0900

    poker

---
+++ poker.py
@@ -0,0 +1,49 @@
+import random
+
+def poker(number):
+  first = number[0:1]
+  second = number[1:2]
+  third = number[2:3]
+  firth = number[3:4]
+  
+  if first == second == third == firth:
+    print('Four Card')
+  elif first == second == third:  #three card
+    print('Three Card')
+  elif second == third == firth:
+    print('Three Card')
+  elif first == second == firth:
+    print('Three Card')
+  elif first == second and third == firth: #Two Pair
+    print('Two Pair')
+  elif first == third and second == firth: #Two Pair
+    print('Two Pair')
+  elif first == firth and second == third: #Two Pair
+    print('Two Pair')
+
+  elif first == second:#Owe Pair
+    print('One Pair')
+  elif first == third:
+    print('One Pair')
+  elif first == firth:
+    print('One Pair')
+  elif second == third:
+    print('One Pair')
+  elif second == firth:
+    print('One Pair')
+  elif third == firth:
+    print('One Pair')
+  else:
+    print('No Pair')
+
+
+number = random.randrange(1000,10000)
+
+print(number)
+
+number = str(number)
+
+poker(number)
+    
+
+ 
--- python10.py
+++
@@ -1,9 +0,0 @@
-money,buy_stock,sell_stock = [int(input()) for i in range(3)]
-
-total_buy = money/buy_stock
-
-total_sell = total_buy * sell_stock
-
-capital = total_sell-money
-
-print(int(capital))
