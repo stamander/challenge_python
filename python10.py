ac3b56f73c984710fb422e307791082b206c0a75
stamander <iida.atsushi.fh@gmail.com>
2020-10-26 22:01:22 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,21 +1,2 @@
-
-
-n= 0
-
-while n < 10000:
-
-  if n % 3==0:
-
-    total_price = n+n
-    print(str(n) + '*')
-
-
-    
-  else:
-
-    print(n)
-  n+=1
-
-
-print(total_price)
+print(1342/1650)
 
