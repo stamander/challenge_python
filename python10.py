92c532e22009042cbc435f07b81207010fc0909b
stamander <iida.atsushi.fh@gmail.com>
2020-10-30 07:45:24 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,4 +1,32 @@
-a,b,c = [input() for i in range(3)]
+day,buy_price,sell_price = list(map(int, input().split())) 
 
 
-print(b+'/'+c+'/'+a)
+
+
+
+stock = [int(input()) for i in range(day)]
+
+have_stock = 0
+benefit = 0
+
+
+for stock_price in stock:
+  if stock_price < buy_price:
+    have_stock+=1
+    benefit-=buy_price
+  elif stock_price > sell_price:
+    have_stock=0
+    benefit+=have_stock*sell_price
+  elif buy_price <= stock_price >= sell_price:
+    have_stock = have_stock
+    benefit = benefit
+
+  
+
+
+print(benefit)
+
+
+
+
+
