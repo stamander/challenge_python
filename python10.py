b7a1e2356dce578f179521bd847a6daa122b5853
stamander <iida.atsushi.fh@gmail.com>
2020-10-30 10:31:52 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,31 +1,8 @@
-day,buy_price,sell_price = list(map(int, input().split())) 
+weight = int(input)
 
+tota_weight = weight*150
 
-
-
-
-stock = [int(input()) for i in range(day)]
-
-have_stock = 0
-benefit = 0
-
-
-for stock_price in stock:
-  if stock_price < buy_price:
-    have_stock+=1
-    benefit-=buy_price
-  elif stock_price > sell_price:
-    have_stock=0
-    benefit+=have_stock*sell_price
-  elif buy_price <= stock_price >= sell_price:
-    have_stock = have_stock
-    benefit = benefit
-
-  
-
-
-print(benefit)
-
+print(total_weight)
 
 
 
