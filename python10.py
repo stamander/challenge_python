e1301e37225c0af36626e961cc40a64da6d868ef
stamander <iida.atsushi.fh@gmail.com>
2020-10-31 11:11:04 +0900

    Update python10.py

---
+++ python10.py
@@ -0,0 +1,22 @@
+height,wide,x= list(map(int, input().split()))
+s = ''
+
+
+for word in range(height):
+  s +=input()
+for word in range(len(s)):
+  print(s[i],end = '')
+  if (i+1)%x== 0 or i+1 == len(s):
+    print()
+
+
+
+# h, w, x = map(int, input().split())
+# s = ''
+# for i in range(h):
+#     s += input()
+# for i in range(len(s)):
+#     print(s[i], end='')
+#     if (i+1) % x == 0 or i+1 == len(s):
+#         print()
+ 
