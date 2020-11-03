681070a281842fa55455bf26e23aabc1a275a242
stamander <iida.atsushi.fh@gmail.com>
2020-11-03 11:53:00 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,26 +1,7 @@
-month = int(input())
+n = [4, 0, 5, -1, 3, 10, 6, -8]
 
-if month == 1:
-  print('January')
-elif month == 2:
-  print('February')
-elif month == 3:
-  print('March')
-elif month == 4:
-  print('April')
-elif month == 5:
-  print('May')
-elif month == 6:
-  print('June')
-elif month == 7:
-  print('July')
-elif month == 8:
-  print('August')
-elif month == 9:
-  print('September')
-elif month == 10:
-  print('October')
-elif month == 11:
-  print('November')
-elif month == 12:
-  print('December')
+for i in n:
+
+  if i >= 5:
+    
+    print(i)
