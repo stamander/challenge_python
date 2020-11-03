f096edaa3b358a5279af04d99a54917f91e2ea18
stamander <iida.atsushi.fh@gmail.com>
2020-11-03 11:44:10 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,5 +1,26 @@
-N = int(input())
-s = [input() for i in range(N)]
+month = int(input())
 
-for namber in s:
-  print(namber)
+if month == 1:
+  print('January')
+elif month == 2:
+  print('February')
+elif month == 3:
+  print('March')
+elif month == 4:
+  print('April')
+elif month == 5:
+  print('May')
+elif month == 6:
+  print('June')
+elif month == 7:
+  print('July')
+elif month == 8:
+  print('August')
+elif month == 9:
+  print('September')
+elif month == 10:
+  print('October')
+elif month == 11:
+  print('November')
+elif month == 12:
+  print('December')
