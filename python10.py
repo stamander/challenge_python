7092b771f857b14222447dc4164142c3045b2c69
stamander <iida.atsushi.fh@gmail.com>
2020-11-05 08:47:38 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,12 +1,3 @@
-result = input()
+N= int(input())
 
-prize = s = input().split()
-
-if result == '3':
-  print(prize[2])
-
-elif result == '2':
-  print(prize[1])
-
-else:
-  print(prize[0])
+print int((N * (N - 1) / 2))
