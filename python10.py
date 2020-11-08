4a002715809c36f5230c9695d5f1d39764e57a04
stamander <iida.atsushi.fh@gmail.com>
2020-11-08 17:02:15 +0900

    Update python10.py

--- python10.py
+++ python10.py
@@ -1,3 +1,14 @@
-n = int(input())
+time = int(input())
+result = 0
 
-print(n-1)
+for i in range(time):
+  number = input()
+  array = number.split()
+
+  if array[0]==array[1]:
+    result += int(array[0])*int(array[1])
+  else:
+    result +=int(array[0])+int(array[1])
+
+print(result)
+
