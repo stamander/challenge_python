b764ed13b4b295dbd820f34f19952b7e002f2439
stamander <iida.atsushi.fh@gmail.com>
2020-11-10 06:49:53 +0900

    修正

---
+++ string_slice.py
@@ -0,0 +1,14 @@
+a,b = list(map(int, input().split()))
+
+string = input()
+
+
+
+print(string[a-1:b])
+
+
+
+# nums = input().split()
+# string = input()
+
+# print(string[int(nums[0]) - 1:int(nums[1])])
