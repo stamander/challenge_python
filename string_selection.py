205060cda205fefb78226686b7a022b18fdbdc7a
stamander <iida.atsushi.fh@gmail.com>
2020-11-14 23:10:35 +0900

    Update string_selection.py

--- string_selection.py
+++ string_selection.py
@@ -3,4 +3,4 @@ string = input()
 
 print(string[0:int(nums[0]) - 1], end='')
 # print(string[int(nums[0]) - 1:int(nums[1])].upper(), end='')
-print(string[int(nums[1]):]
+print(string[int(nums[1]):])
