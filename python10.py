2059977bd1f7819fb1e8641a74fe311dfd0cb9c1
stamander <iida.atsushi.fh@gmail.com>
2020-11-20 21:21:13 +0900

    Update python10.py

---
+++ python10.py
@@ -0,0 +1,13 @@
+t= int(input())
+
+if t  <=24:
+  print(1)
+elif 24<t and 48 >=t:
+  print(2)
+elif 48<t and 72 >=t:
+  print(3)
+elif 72<t and 96 >=t:
+  print(4)
+elif 96<t and 96+24 >=t:
+  print(5)
+
