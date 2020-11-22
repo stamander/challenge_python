35e20d5e4f2025a62a9fefb2f20777f8e7738db5
stamander <iida.atsushi.fh@gmail.com>
2020-11-22 14:29:10 +0900

    Update ikko_tobashi.py

--- ikko_tobashi.py
+++ ikko_tobashi.py
@@ -1,3 +1,24 @@
-string = input()
+# string = input()
 
-print(string[0] + ' ' + string[1])
+# print(string[0] + ' ' + string[1])
+
+
+
+# 入力例1
+# 2
+# read
+
+
+
+# スライス使った一つ飛ばし
+
+# nに数を代入
+n = int(input()) - 1
+# stringに代入
+string = input() 
+
+if n + 1 < len(string):
+    print(string[n] + ' ' + string[n + 1])
+
+# 出力例1
+# e a
