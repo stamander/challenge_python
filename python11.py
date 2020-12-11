99cbe4ffc9ecb54f968fb3e382b69120972a84fb
stamander <iida.atsushi.fh@gmail.com>
2020-12-11 21:43:20 +0900

    Update python11.py

--- python11.py
+++ python11.py
@@ -1,4 +1,29 @@
-s = int(input())
-t = input()
+N = int(input())
 
-print(t[:s])
+s = [int(input()) for i in range(N)]
+
+ans = 0
+i =1
+
+for st in s:
+  ans +=abs(st-i)
+
+  i=st
+
+print(ans)
+
+# print(sum(s)-1)
+
+
+
+# n = gets.to_i
+
+# ans = 0
+# i = 1
+# n.times{
+#     a = gets.to_i
+#     ans += (a-i).abs
+#     i = a
+# }
+
+# puts ans
